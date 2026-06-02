1b76231f7373aafeef405837f39194c0e802385c
Kyle Givler <xvegan88x@gmail.com>
2026-06-02 11:36:40 -0400

    Reset favor on ideo change

---
+++ Source/Patches/PawnIdeoTrackerPatches.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace TheGodsAreReal.Patches
+{
+    [HarmonyPatch(typeof(Pawn_IdeoTracker), nameof(Pawn_IdeoTracker.SetIdeo))]
+    public static class PawnIdeoTrackerPatches_SetIdeo
+    {
+        public static void Prefix(Pawn_IdeoTracker __instance, Ideo ideo, Pawn ___pawn)
+        {
+            if (___pawn == null)
+                return;
+
+            if(__instance.Ideo != ideo)
+            {
+                var favorTracker = Find.World.GetComponent<WorldComponent_FavorTracker>();
+                if (favorTracker != null)
+                {
+                    favorTracker.ResetFavor(___pawn);
+                }
+            }
+        }
+    }
+}
