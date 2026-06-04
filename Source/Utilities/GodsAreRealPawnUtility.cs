41cf53d5201033a3bc1157bf8e013e9fdb40b399
Kyle Givler <xvegan88x@gmail.com>
2026-06-04 17:12:53 -0400

    Only care about humans

--- Source/Utilities/GodsAreRealPawnUtility.cs
+++ Source/Utilities/GodsAreRealPawnUtility.cs
@@ -1,11 +1,34 @@
-﻿using RimWorld;
-using System;
+﻿/**
+BSD 2-Clause License
+
+Copyright (c) 2026, Kyle Givler
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **/
+
+using RimWorld;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Verse;
-using Verse.Noise;
 
 namespace TheGodsAreReal.Utilities
 {
@@ -20,7 +43,25 @@ namespace TheGodsAreReal.Utilities
             {
                 Pawn pawn = allLivingPawns[i];
 
-                if (pawn.IsColonist || pawn.IsPrisonerOfColony || pawn.IsSlaveOfColony)
+                if ((pawn.IsColonist || pawn.IsPrisonerOfColony || pawn.IsSlaveOfColony) && pawn.RaceProps.Humanlike)
+                {
+                    result.Add(pawn);
+                }
+            }
+
+            return result;
+        }
+
+        public static List<Pawn> GetAllColonyPawnsExceptPrisoners()
+        {
+            List<Pawn> allLivingPawns = PawnsFinder.AllMapsCaravansAndTravellingTransporters_Alive;
+            List<Pawn> result = new List<Pawn>(allLivingPawns.Count);
+
+            for (int i = 0; i < allLivingPawns.Count; i++)
+            {
+                Pawn pawn = allLivingPawns[i];
+
+                if ((pawn.IsColonist || pawn.IsSlaveOfColony) && pawn.RaceProps.Humanlike)
                 {
                     result.Add(pawn);
                 }
@@ -52,7 +93,7 @@ namespace TheGodsAreReal.Utilities
             for (int i = 0; i < allSpawned.Count; i++)
             {
                 Pawn p = allSpawned[i];
-                if (p.IsColonist || p.IsPrisonerOfColony)
+                if ((p.IsColonist || p.IsSlaveOfColony) && p.RaceProps.Humanlike)
                 {
                     result.Add(p);
                 }
@@ -76,7 +117,7 @@ namespace TheGodsAreReal.Utilities
             List<Pawn> result = new List<Pawn>();
             for (int i = 0; i < pawns.Count; i++)
             {
-                if (pawns[i].IsColonist && pawns[i].Downed)
+                if ((pawns[i].IsColonist || pawns[i].IsSlaveOfColony) && pawns[i].Downed && pawns[i].RaceProps.Humanlike)
                     result.Add(pawns[i]);
             }
             return result;
@@ -84,13 +125,13 @@ namespace TheGodsAreReal.Utilities
 
         public static List<Pawn> GetCapableColonyPawns(Map map)
         {
-            var freeColonists = map.mapPawns.FreeColonists;
+            var freeColonists = map.mapPawns.AllPawnsSpawned;
             List<Pawn> result = new List<Pawn>(freeColonists.Count);
 
             for (int i = 0; i < freeColonists.Count; i++)
             {
                 Pawn p = freeColonists[i];
-                if (p.Awake() && !p.Downed && !p.InMentalState)
+                if (p.Awake() && !p.Downed && !p.InMentalState && (p.IsColonist || p.IsSlaveOfColony) && p.RaceProps.Humanlike)
                 {
                     result.Add(p);
                 }
