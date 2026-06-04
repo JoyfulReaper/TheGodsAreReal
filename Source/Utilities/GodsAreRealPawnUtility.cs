a0b3c5c8d88e04fe8a5fe6b0d9d5b0f344af7fb5
Kyle Givler <xvegan88x@gmail.com>
2026-06-04 16:17:51 -0400

    Add a pawn utility
    Easier to use than the vanilla code

--- Source/Utilities/GodsAreRealPawnUtility.cs
+++ Source/Utilities/GodsAreRealPawnUtility.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Verse;
+using Verse.Noise;
 
 namespace TheGodsAreReal.Utilities
 {
@@ -12,14 +13,115 @@ namespace TheGodsAreReal.Utilities
     {
         public static List<Pawn> GetAllColonyPawns()
         {
-            List<Pawn> colonistsAndSlaves = PawnsFinder.AllMapsCaravansAndTravellingTransporters_Alive_Colonists;
-            List<Pawn> prisoners = PawnsFinder.AllMapsCaravansAndTravellingTransporters_Alive_PrisonersOfColony;
+            List<Pawn> allLivingPawns = PawnsFinder.AllMapsCaravansAndTravellingTransporters_Alive;
+            List<Pawn> result = new List<Pawn>(allLivingPawns.Count);
 
-            List<Pawn> result = new List<Pawn>(colonistsAndSlaves.Count + prisoners.Count);
+            for (int i = 0; i < allLivingPawns.Count; i++)
+            {
+                Pawn pawn = allLivingPawns[i];
 
-            result.AddRange(colonistsAndSlaves);
-            result.AddRange(prisoners);
+                if (pawn.IsColonist || pawn.IsPrisonerOfColony || pawn.IsSlaveOfColony)
+                {
+                    result.Add(pawn);
+                }
+            }
 
+            return result;
+        }
+
+        public static List<Pawn> GetAllFreeColonyPawns()
+        {
+            return PawnsFinder.AllMapsCaravansAndTravellingTransporters_Alive_FreeColonists;
+        }
+
+        public static List<Pawn> GetAllColonyPrisoners()
+        {
+            return PawnsFinder.AllMapsCaravansAndTravellingTransporters_Alive_PrisonersOfColony;
+        }
+
+        public static List<Pawn> GetAllColonyPrisonersOnMap(Map map)
+        {
+            return map.mapPawns.PrisonersOfColonySpawned;
+        }
+
+        public static List<Pawn> GetAllColonyPawnsOnMap(Map map)
+        {
+            var allSpawned = map.mapPawns.AllPawnsSpawned;
+            List<Pawn> result = new List<Pawn>();
+
+            for (int i = 0; i < allSpawned.Count; i++)
+            {
+                Pawn p = allSpawned[i];
+                if (p.IsColonist || p.IsPrisonerOfColony)
+                {
+                    result.Add(p);
+                }
+            }
+            return result;
+        }
+
+        public static List<Pawn> GetAllFreePawnsOnMap(Map map)
+        {
+            return map.mapPawns.FreeColonists;
+        }
+
+        public static List<Pawn> GetAllSlavePawnsOnMap(Map map)
+        {
+            return map.mapPawns.SlavesOfColonySpawned;
+        }
+
+        public static List<Pawn> GetDownedColonyPawns(Map map)
+        {
+            var pawns = map.mapPawns.AllPawnsSpawned;
+            List<Pawn> result = new List<Pawn>();
+            for (int i = 0; i < pawns.Count; i++)
+            {
+                if (pawns[i].IsColonist && pawns[i].Downed)
+                    result.Add(pawns[i]);
+            }
+            return result;
+        }
+
+        public static List<Pawn> GetCapableColonyPawns(Map map)
+        {
+            var freeColonists = map.mapPawns.FreeColonists;
+            List<Pawn> result = new List<Pawn>(freeColonists.Count);
+
+            for (int i = 0; i < freeColonists.Count; i++)
+            {
+                Pawn p = freeColonists[i];
+                if (p.Awake() && !p.Downed && !p.InMentalState)
+                {
+                    result.Add(p);
+                }
+            }
+            return result;
+        }
+
+        public static bool IsPawnConsciousAndActive(Pawn p)
+        {
+            return p.Spawned && !p.Dead && p.health.capacities.CapableOf(PawnCapacityDefOf.Manipulation) && !p.Downed;
+        }
+
+        public static List<Pawn> GetPawnsInMentalState(Map map, MentalStateDef stateDef)
+        {
+            return map.mapPawns.AllPawnsSpawned.Where(p => p.InMentalState && p.MentalStateDef == stateDef).ToList();
+        }
+
+        public static List<Pawn> GetPawnsCurrentlyPraying(Map map)
+        {
+            return map.mapPawns.FreeColonists.Where(p => p.CurJobDef == JobDefOf.MeditatePray).ToList();
+        }
+
+        public static List<Pawn> GetColonyPawnsByFaction(Map map, Faction faction)
+        {
+            var pawns = map.mapPawns.AllPawnsSpawned;
+            List<Pawn> result = new List<Pawn>();
+            for (int i = 0; i < pawns.Count; i++)
+            {
+                if (pawns[i].Faction == faction)
+                    result.Add(pawns[i]);
+            }
             return result;
         }
     }
