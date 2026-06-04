160cee21ec183b5d34d2d5d866db7a7adac0b1e1
Kyle Givler <xvegan88x@gmail.com>
2026-06-04 00:38:54 -0400

    Add hook for conversion attempts

---
+++ Source/Patches/InteractionWorker_ConvertIdeoAttemptPatches.cs
@@ -0,0 +1,67 @@
+﻿/**
+BSD 2-Clause License
+
+Copyright (c) 2026, Kyle Givler
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **/
+
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace TheGodsAreReal.Patches
+{
+    [HarmonyPatch(typeof(InteractionWorker_ConvertIdeoAttempt), nameof(InteractionWorker_ConvertIdeoAttempt.Interacted))]
+    public static class Patch_InteractionWorker_ConvertIdeoAttempt_Interacted
+    {
+        public static void Postfix(Pawn initiator, Pawn recipient)
+        {
+            if (initiator == null || recipient == null)
+                return;
+
+            if (!initiator.RaceProps.Humanlike || !recipient.RaceProps.Humanlike)
+                return;
+
+            var tracker = Find.World?.GetComponent<WorldComponent_FavorTracker>();
+            if (tracker == null)
+                return;
+
+            // Reward the converter
+            if (initiator.IsColonist || initiator.IsSlave)
+            {
+                tracker.AddFavor(initiator, 2f, showMote: true);
+            }
+
+            // Punish the convertee
+            if (recipient.IsColonist || recipient.IsSlave)
+            {
+                tracker.AddFavor(recipient, -1f, showMote: true);
+            }
+
+            if (Prefs.DevMode)
+            {
+                Log.Message($"[TheGodsAreReal] Conversion attempt: {initiator.LabelShort} preached to {recipient.LabelShort}.");
+            }
+        }
+    }
+}
