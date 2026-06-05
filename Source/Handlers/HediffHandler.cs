dcaad9ed91e9a0f3116ef0c683eabf41c9aeda32
Kyle Givler <xvegan88x@gmail.com>
2026-06-04 23:10:54 -0400

    Add EventUtility, EventHandler, Hediff Handler

---
+++ Source/Handlers/HediffHandler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheGodsAreReal.Utilities;
+using UnityEngine;
+using Verse;
+
+namespace TheGodsAreReal.Handlers
+{
+    public static class HediffHandler
+    {
+        // Hediff just used for testing right now
+        private static readonly HediffDef _divineTouchDef = HediffDef.Named("TheGodsAreReal_DivineTouch");
+
+        internal static void ApplyHediffs()
+        {
+            foreach (Pawn pawn in GodsAreRealPawnUtility.GetAllColonyPawns())
+            {
+                if (pawn == null || pawn.Dead || !pawn.RaceProps.Humanlike || pawn.Ideo?.KeyDeityName == null)
+                    continue;
+
+                UpdatePawnDivineHediff(pawn);
+            }
+        }
+
+        private static void UpdatePawnDivineHediff(Pawn pawn)
+        {
+            var tracker = Find.World.GetComponent<WorldComponent_FavorTracker>();
+            if (tracker == null)
+                return;
+
+            float favor = tracker.GetFavor(pawn);
+            float normalizedSeverity = Mathf.Clamp01((favor + 100f) / 200f);
+            // Map the favor score (-100.0 to 100.0) to a C# normalized float scale (0.0 to 1.0)
+            // -100 favor becomes 0.0 severity (Pure Wrath)
+            //    0 favor becomes 0.5 severity (Neutral / Hidden)
+            // +100 favor becomes 1.0 severity (Pure Grace)
+
+            // Look for an existing instance of our Hediff on the pawn
+            Hediff existingHediff = pawn.health?.hediffSet?.GetFirstHediffOfDef(_divineTouchDef);
+
+            if (existingHediff != null)
+            {
+                // Update the severity level dynamically as their behavior changes
+                existingHediff.Severity = normalizedSeverity;
+            }
+            else
+            {
+                // If they don't have the hediff yet, create it and inject it into their health tracker
+                Hediff newHediff = HediffMaker.MakeHediff(_divineTouchDef, pawn);
+                newHediff.Severity = normalizedSeverity;
+                pawn.health?.AddHediff(newHediff);
+            }
+        }
+    }
+}
---
+++ Source/Utilities/GodsAreRealEventUtility.cs
@@ -0,0 +1,66 @@
+﻿/**
+BSD 2-Clause License
+
+Copyright (c) 2026, Kyle Givler
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **/
+
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace TheGodsAreReal.Utilities
+{
+    internal class GodsAreRealEventUtility
+    {
+        private const float favorForWrath = -70f;
+        private const float favorForBlessing = 80f;
+
+        public static void TriggerDivineEvent(Ideo ideo)
+        {
+            var tracker = Find.World.GetComponent<WorldComponent_FavorTracker>();
+            float avgFavor = tracker.GetIdeoFavor(ideo);
+
+            var members = GodsAreRealPawnUtility.GetAllIdeoMembers(ideo);
+
+            if (avgFavor < favorForWrath)
+            {
+                // Example Wrath Event
+                Pawn target = members.RandomElementWithFallback();
+                if (target != null && target.Spawned)
+                {
+                    GenExplosion.DoExplosion(target.Position, target.Map, 1.5f, DamageDefOf.Flame, null);
+                    Messages.Message("The Gods are angry at your lack of faith!", target, MessageTypeDefOf.NegativeEvent);
+                }
+            }
+            else if (avgFavor > favorForBlessing)
+            {
+                // Example Blessing Event
+                Pawn target = members.RandomElementWithFallback();
+                target?.mindState?.inspirationHandler?.TryStartInspiration(InspirationDefOf.Inspired_Creativity);
+                Messages.Message($"The Gods have blessed your colony! {target.LabelShort} has become inspired!", target, MessageTypeDefOf.PositiveEvent);
+            }
+        }
+    }
+}
