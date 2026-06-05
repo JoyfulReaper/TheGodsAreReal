0afd57c6b6047b615b56c758610ce358729c9e20
Kyle Givler <xvegan88x@gmail.com>
2026-06-04 23:12:22 -0400

    0.0.19
    0.0.19

--- Source/Settings/TheGodsAreRealSettings.cs
+++ Source/Settings/TheGodsAreRealSettings.cs
@@ -36,7 +36,7 @@ namespace TheGodsAreReal
 {
     public class TheGodsAreRealSettings : ModSettings
     {
-        private string _version = "0.0.18";
+        private string _version = "0.0.19";
         private bool _alwaysShowMotes = false;
         private static Vector2 _scrollPos = Vector2.zero;
         private const int _buttonCount = 0;
